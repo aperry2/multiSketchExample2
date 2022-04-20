0bd1ed68dac396c69e0c4c129ff0b0eddcd508f3
aperry2 <31574553+aperry2@users.noreply.github.com>
2022-04-20 11:27:38 -0500

    Add files via upload

---
+++ greenCircle.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(300, 300);
+}
+
+function draw() {
+  background(0);
+  fill('green');
+  circle(width / 2, height / 2, 250);
+}
---
+++ redLine.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(200, 200);
+}
+
+function draw() {
+  background(220);
+  stroke('red');
+  strokeWeight(5);
+  line(0, 0, width, height);
+}
